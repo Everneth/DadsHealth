40d0fc77939d881ccc4895de26620def1c78b343
Mike White <mike.wildcats@gmail.com>
2019-04-01 16:35:59 -0400

    Initial Heartbeat service

---
+++ DadsHealth/Services/HeartBeatService.cs
@@ -0,0 +1,19 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DadsHealth.Services
+{
+    public class HeartBeatService
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly IServiceProvider _services;
+        public HeartBeatService(DiscordSocketClient client, IServiceProvider services)
+        {
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _services = services;
+        }
+    }
+}
